bcf89c37424572f43064a5322dc7cc2047eab2a6
Omar <omarzaldivar07@aragon.unam.mx>
2021-11-06 09:56:16 -0600

    Se agrego parte visual al problema 2, falta implementar funcionamiento

---
+++ Problema 2/src/application/VistaController.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.Label;
+
+import javafx.scene.image.ImageView;
+
+public class VistaController {
+	@FXML
+	private ImageView img;
+	@FXML
+	private TextField txtExpresion;
+	@FXML
+	private Label lblResultado;
+	@FXML
+	private Button btnCalcular;
+	@FXML
+	private Button btnSalir;
+
+}
