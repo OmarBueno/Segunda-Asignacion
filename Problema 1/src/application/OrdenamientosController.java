3a9c31557ccdc6fb309d967d62477fbdafd1adc0
Omar <omarzaldivar07@aragon.unam.mx>
2021-11-07 19:38:40 -0600

    Se separaron las funciones del problema 1, se redujo codigo

--- Problema 1/src/application/OrdenamientosController.java
+++ Problema 1/src/application/OrdenamientosController.java
@@ -12,8 +12,8 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import fes.aragon.except.IndiceFueraDeRango;
+import fes.aragon.herramientas.*;
 import fes.aragon.utilerias.dinamicas.listasimple.ListaSimple;
-import fes.aragon.utilerias.dinamicas.pila.Pila;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 
@@ -76,246 +76,70 @@ public class OrdenamientosController implements Initializable {
 
 	@FXML
 	public void evento(ActionEvent event) {
+		// Metodo burbuja
 		scheduledExecutorServiceB = Executors.newSingleThreadScheduledExecutor();
 		scheduledExecutorServiceB.scheduleAtFixedRate(() -> {
 			Platform.runLater(() -> {
-				for (int i = 0; i < listaB.getLongitud(); i++) {
-					for (int j = listaB.getLongitud() - 1; j > i; j--) {
-						try {
-							if (listaB.obtenerNodo(j - 1) > listaB.obtenerNodo(j)) {
-								Integer aux = listaB.obtenerNodo(j - 1);
-								listaB.asignar(listaB.obtenerNodo(j), j - 1);
-								listaB.asignar(aux, j);
-								String tmpEstilo = seriesB.getData().get(j).getNode().getStyle();
-								String tmpEstiloDos = seriesB.getData().get(j - 1).getNode().getStyle();
-								seriesB.getData().get(j).getNode().setStyle(tmpEstiloDos);
-								seriesB.getData().get(j - 1).getNode().setStyle(tmpEstilo);
-								seriesB.getData().get(j).setYValue(listaB.obtenerNodo(j));
-								seriesB.getData().get(j - 1).setYValue(listaB.obtenerNodo(j - 1));
-								break;
-							}
-						} catch (IndiceFueraDeRango e) {
-							e.printStackTrace();
-						}
-					}
-				}
+				Ordenamientos.burbuja(listaB, seriesB);
 			});
 		}, 0, 1, TimeUnit.SECONDS);
+		// Metodo seleccion
 		scheduledExecutorServiceS = Executors.newSingleThreadScheduledExecutor();
 		scheduledExecutorServiceS.scheduleAtFixedRate(() -> {
 			Platform.runLater(() -> {
-				int menor;
-				int k;
-				for (int i = 0; i < listaS.getLongitud() - 1; i++) {
-					try {
-						menor = listaS.obtenerNodo(i);
-						k = i;
-						for (int j = i + 1; j < listaS.getLongitud(); j++) {
-							if (listaS.obtenerNodo(j) < menor) {
-								menor = listaS.obtenerNodo(j);
-								k = j;
-							}
-							break;
-						}
-						listaS.asignar(listaS.obtenerNodo(i), k);
-						listaS.asignar(menor, i);
-						String tmpEstilo = seriesS.getData().get(i).getNode().getStyle();
-						String tmpEstiloDos = seriesS.getData().get(k).getNode().getStyle();
-						seriesS.getData().get(k).getNode().setStyle(tmpEstilo);
-						seriesS.getData().get(i).getNode().setStyle(tmpEstiloDos);
-						seriesS.getData().get(k).setYValue(listaS.obtenerNodo(k));
-						seriesS.getData().get(i).setYValue(listaS.obtenerNodo(i));
-					} catch (IndiceFueraDeRango e) {
-						e.printStackTrace();
-					}
-				}
+				Ordenamientos.seleccion(listaS, seriesS);
 			});
 		}, 0, 1, TimeUnit.SECONDS);
+		// Metodo insercion
 		scheduledExecutorServiceI = Executors.newSingleThreadScheduledExecutor();
 		scheduledExecutorServiceI.scheduleAtFixedRate(() -> {
 			Platform.runLater(() -> {
-				int aux;
-				int k;
-				for (int i = 1; i < listaI.getLongitud(); i++) {
-					try {
-						aux = listaI.obtenerNodo(i);
-						k = i - 1;
-						while (k >= 0 && aux < listaI.obtenerNodo(k)) {
-							listaI.asignar(listaI.obtenerNodo(k), k + 1);
-							// String tmpEstilo = seriesI.getData().get(k).getNode().getStyle();
-							// String tmpEstiloDos = seriesI.getData().get(k + 1).getNode().getStyle();
-							// seriesI.getData().get(k + 1).getNode().setStyle(tmpEstilo);
-							// seriesI.getData().get(k).getNode().setStyle(tmpEstiloDos);
-							seriesI.getData().get(k + 1).setYValue(listaI.obtenerNodo(k + 1));
-							// series.getData().get(k).setYValue(aux);
-							k = k - 1;
-							break;
-						}
-						listaI.asignar(aux, k + 1);
-						// String tmpEstiloDos = seriesI.getData().get(aux).getNode().getStyle();
-						// seriesI.getData().get(k + 1).getNode().setStyle(tmpEstiloDos);
-						seriesI.getData().get(k + 1).setYValue(listaI.obtenerNodo(k + 1));
-					} catch (IndiceFueraDeRango e) {
-						e.printStackTrace();
-					}
-				}
+				Ordenamientos.insercion(listaI, seriesI);
 			});
 		}, 0, 1, TimeUnit.SECONDS);
+		// Metodo quick sort recursivo
 		scheduledExecutorServiceQ = Executors.newSingleThreadScheduledExecutor();
 		scheduledExecutorServiceQ.scheduleAtFixedRate(() -> {
 			Platform.runLater(() -> {
 				try {
-					reduceRecursivo(0, listaQ.getLongitud() - 1);
+					Ordenamientos.reduceRecursivo(0, listaQ.getLongitud() - 1, listaQ, seriesQ);
 				} catch (IndiceFueraDeRango e) {
 					e.printStackTrace();
 				}
 			});
 		}, 0, 1, TimeUnit.SECONDS);
+		// Metodo quick sort iterativo
 		scheduledExecutorServiceQI = Executors.newSingleThreadScheduledExecutor();
 		scheduledExecutorServiceQI.scheduleAtFixedRate(() -> {
 			Platform.runLater(() -> {
-				try {
-					int tope, ini, fin, pos = 0;
-					Pila<Integer> pMenor = new Pila<>();
-					Pila<Integer> pMayor = new Pila<>();
-					tope = 1;
-					pMenor.insertar(0);
-					pMayor.insertar(listaQI.getLongitud() - 1);
-					while (tope > 0) {
-						ini = pMenor.extraer();
-						fin = pMayor.extraer();
-						tope--;
-						pos = reduceItertivo(ini, fin, pos);
-						if (ini < (pos - 1)) {
-							tope++;
-							pMenor.insertar(ini);
-							pMayor.insertar(pos - 1);
-						}
-						if (fin > (pos + 1)) {
-							tope++;
-							pMenor.insertar(pos + 1);
-							pMayor.insertar(fin);
-						}
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				Ordenamientos.rapidoIterativo(listaQI, seriesQI);
 			});
 		}, 0, 1, TimeUnit.SECONDS);
 		scheduledExecutorServiceSa = Executors.newSingleThreadScheduledExecutor();
 		scheduledExecutorServiceSa.scheduleAtFixedRate(() -> {
 			Platform.runLater(() -> {
-				try {
-					int izquierda = 1;
-					int derecha = listaSa.getLongitud() - 1;
-					int k = listaSa.getLongitud() - 1;
-					while (derecha >= izquierda) {
-						for (int i = derecha; i >= izquierda; i--) {
-							if (listaSa.obtenerNodo(i - 1) > listaSa.obtenerNodo(i)) {
-								Integer aux = listaSa.obtenerNodo(i - 1);
-								listaSa.asignar(listaSa.obtenerNodo(i), i - 1);
-								listaSa.asignar(aux, i);
-								String tmpEstilo = seriesSa.getData().get(i - 1).getNode().getStyle();
-								String tmpEstiloDos = seriesSa.getData().get(i).getNode().getStyle();
-								seriesSa.getData().get(i - 1).getNode().setStyle(tmpEstilo);
-								seriesSa.getData().get(i).getNode().setStyle(tmpEstiloDos);
-								seriesSa.getData().get(i - 1).setYValue(listaSa.obtenerNodo(i - 1));
-								seriesSa.getData().get(i).setYValue(listaSa.obtenerNodo(i));
-								k = i;
-							}
-						}
-						izquierda = k + 1;
-						for (int i = izquierda; i <= derecha; i++) {
-							if (listaSa.obtenerNodo(i - 1) > listaSa.obtenerNodo(i)) {
-								Integer aux = listaSa.obtenerNodo(i - 1);
-								listaSa.asignar(listaSa.obtenerNodo(i), i - 1);
-								listaSa.asignar(aux, i);
-								String tmpEstilo = seriesSa.getData().get(i - 1).getNode().getStyle();
-								String tmpEstiloDos = seriesSa.getData().get(i).getNode().getStyle();
-								seriesSa.getData().get(i - 1).getNode().setStyle(tmpEstilo);
-								seriesSa.getData().get(i).getNode().setStyle(tmpEstiloDos);
-								seriesSa.getData().get(i - 1).setYValue(listaSa.obtenerNodo(i - 1));
-								seriesSa.getData().get(i).setYValue(listaSa.obtenerNodo(i));
-								k = i;
-							}
-						}
-						derecha = k - 1;
-						break;
-					}
-				} catch (IndiceFueraDeRango e) {
-					e.printStackTrace();
-				}
+				Ordenamientos.sacudida(listaSa, seriesSa);
 			});
 		}, 0, 1, TimeUnit.SECONDS);
 	}
 
+	@SuppressWarnings("unchecked")
 	@FXML
 	public void aleatorios(ActionEvent event) throws IndiceFueraDeRango {
 		numerosAleatorios();
-		seriesB.getData().clear();
-		seriesS.getData().clear();
-		seriesSa.getData().clear();
-		seriesI.getData().clear();
-		seriesQ.getData().clear();
-		seriesQI.getData().clear();
-		for (int i = 0; i < listaB.getLongitud(); i++) {
-			seriesB.getData().add(new XYChart.Data<>(String.valueOf(i), listaB.obtenerNodo(i)));
-			seriesB.getData().get(i).getNode().setStyle(color[new Random().nextInt(4)]);
-			seriesS.getData().add(new XYChart.Data<>(String.valueOf(i), listaB.obtenerNodo(i)));
-			seriesS.getData().get(i).getNode().setStyle(color[new Random().nextInt(4)]);
-			seriesSa.getData().add(new XYChart.Data<>(String.valueOf(i), listaB.obtenerNodo(i)));
-			seriesSa.getData().get(i).getNode().setStyle(color[new Random().nextInt(4)]);
-			seriesI.getData().add(new XYChart.Data<>(String.valueOf(i), listaB.obtenerNodo(i)));
-			seriesI.getData().get(i).getNode().setStyle(color[new Random().nextInt(4)]);
-			seriesQ.getData().add(new XYChart.Data<>(String.valueOf(i), listaB.obtenerNodo(i)));
-			seriesQ.getData().get(i).getNode().setStyle(color[new Random().nextInt(4)]);
-			seriesQI.getData().add(new XYChart.Data<>(String.valueOf(i), listaB.obtenerNodo(i)));
-			seriesQI.getData().get(i).getNode().setStyle(color[new Random().nextInt(4)]);
-		}
-		if (scheduledExecutorServiceB != null) {
-			scheduledExecutorServiceB.shutdown();
-		}
-		if (scheduledExecutorServiceS != null) {
-			scheduledExecutorServiceS.shutdown();
-		}
-		if (scheduledExecutorServiceSa != null) {
-			scheduledExecutorServiceSa.shutdown();
-		}
-		if (scheduledExecutorServiceI != null) {
-			scheduledExecutorServiceI.shutdown();
-		}
-		if (scheduledExecutorServiceQ != null) {
-			scheduledExecutorServiceQ.shutdown();
-		}
-		if (scheduledExecutorServiceQI != null) {
-			scheduledExecutorServiceQI.shutdown();
-		}
+		Herramientas.limpiarSeries(seriesB, seriesS, seriesSa, seriesI, seriesQ, seriesQI);
+		Herramientas.llenarGrafica(listaB, color, seriesB, seriesS, seriesSa, seriesI, seriesQ, seriesQI);
+		Herramientas.apagarHilos(scheduledExecutorServiceB, scheduledExecutorServiceS, scheduledExecutorServiceSa,
+				scheduledExecutorServiceI, scheduledExecutorServiceQ, scheduledExecutorServiceQI);
 	}
 
-	// Event Listener on Button[#btnSalir].onAction
 	@FXML
 	public void eventoSalir(ActionEvent event) {
-		if (scheduledExecutorServiceB != null) {
-			scheduledExecutorServiceB.shutdown();
-		}
-		if (scheduledExecutorServiceS != null) {
-			scheduledExecutorServiceS.shutdown();
-		}
-		if (scheduledExecutorServiceSa != null) {
-			scheduledExecutorServiceSa.shutdown();
-		}
-		if (scheduledExecutorServiceI != null) {
-			scheduledExecutorServiceI.shutdown();
-		}
-		if (scheduledExecutorServiceQ != null) {
-			scheduledExecutorServiceQ.shutdown();
-		}
-		if (scheduledExecutorServiceQI != null) {
-			scheduledExecutorServiceQI.shutdown();
-		}
+		Herramientas.apagarHilos(scheduledExecutorServiceB, scheduledExecutorServiceS, scheduledExecutorServiceSa,
+				scheduledExecutorServiceI, scheduledExecutorServiceQ, scheduledExecutorServiceQI);
 		Platform.exit();
 	}
-
+	//Posible metodo
 	private void numerosAleatorios() throws IndiceFueraDeRango {
 		listaB = new ListaSimple<>();
 		listaS = new ListaSimple<>();
@@ -343,12 +167,7 @@ public class OrdenamientosController implements Initializable {
 		} catch (IndiceFueraDeRango e) {
 			e.printStackTrace();
 		}
-		yAxisB.setLabel("Valor");
-		yAxisB.setAnimated(false);
-		xAxisB.setLabel("Datos");
-		xAxisB.setAnimated(false);
-		areaBurbuja.setTitle("Método Burbuja");
-		areaBurbuja.setAnimated(false);
+		Herramientas.llenarDatos(yAxisB, xAxisB, areaBurbuja, "Burbuja");
 		seriesB = new XYChart.Series<>();
 		seriesB.setName("Conjunto de datos");
 		areaBurbuja.getData().add(seriesB);
@@ -361,12 +180,7 @@ public class OrdenamientosController implements Initializable {
 				e.printStackTrace();
 			}
 		}
-		yAxisS.setLabel("Valor");
-		yAxisS.setAnimated(false);
-		xAxisS.setLabel("Datos");
-		xAxisS.setAnimated(false);
-		areaSeleccion.setTitle("Método Seleccion");
-		areaSeleccion.setAnimated(false);
+		Herramientas.llenarDatos(yAxisS, xAxisS, areaSeleccion, "Seleccion");
 		seriesS = new XYChart.Series<>();
 		seriesS.setName("Conjunto de datos");
 		areaSeleccion.getData().add(seriesS);
@@ -379,12 +193,7 @@ public class OrdenamientosController implements Initializable {
 				e.printStackTrace();
 			}
 		}
-		yAxisI.setLabel("Valor");
-		yAxisI.setAnimated(false);
-		xAxisI.setLabel("Datos");
-		xAxisI.setAnimated(false);
-		areaInsercion.setTitle("Método insercion");
-		areaInsercion.setAnimated(false);
+		Herramientas.llenarDatos(yAxisI, xAxisI, areaInsercion, "Insercion");
 		seriesI = new XYChart.Series<>();
 		seriesI.setName("Conjunto de datos");
 		areaInsercion.getData().add(seriesI);
@@ -397,12 +206,7 @@ public class OrdenamientosController implements Initializable {
 				e.printStackTrace();
 			}
 		}
-		yAxisSa.setLabel("Valor");
-		yAxisSa.setAnimated(false);
-		xAxisSa.setLabel("Datos");
-		xAxisSa.setAnimated(false);
-		areaSacudida.setTitle("Método Sacudida");
-		areaSacudida.setAnimated(false);
+		Herramientas.llenarDatos(yAxisSa, xAxisSa, areaSacudida, "Sacudida");
 		seriesSa = new XYChart.Series<>();
 		seriesSa.setName("Conjunto de datos");
 		areaSacudida.getData().add(seriesSa);
@@ -415,12 +219,7 @@ public class OrdenamientosController implements Initializable {
 				e.printStackTrace();
 			}
 		}
-		yAxisQ.setLabel("Valor");
-		yAxisQ.setAnimated(false);
-		xAxisQ.setLabel("Datos");
-		xAxisQ.setAnimated(false);
-		areaQuick.setTitle("Método Quick Sort");
-		areaQuick.setAnimated(false);
+		Herramientas.llenarDatos(yAxisQ, xAxisQ, areaQuick, "Quick sort recursivo");
 		seriesQ = new XYChart.Series<>();
 		seriesQ.setName("Conjunto de datos");
 		areaQuick.getData().add(seriesQ);
@@ -433,12 +232,7 @@ public class OrdenamientosController implements Initializable {
 				e.printStackTrace();
 			}
 		}
-		yAxisQI.setLabel("Valor");
-		yAxisQI.setAnimated(false);
-		xAxisQI.setLabel("Datos");
-		xAxisQI.setAnimated(false);
-		areaQuickI.setTitle("Método Quick Sort");
-		areaQuickI.setAnimated(false);
+		Herramientas.llenarDatos(yAxisQI, xAxisQI, areaQuickI, "Quick sort iterativo");
 		seriesQI = new XYChart.Series<>();
 		seriesQI.setName("Conjunto de datos");
 		areaQuickI.getData().add(seriesQI);
@@ -452,102 +246,5 @@ public class OrdenamientosController implements Initializable {
 			}
 		}
 
-	}
-
-	public void reduceRecursivo(int ini, int fin) throws IndiceFueraDeRango {
-		int izq = ini;
-		int der = fin;
-		int pos = ini;
-		int aux;
-		boolean band = true;
-		while (band == true) {
-			band = false;
-			while (listaQ.obtenerNodo(pos) <= listaQ.obtenerNodo(der) && pos != der) {
-				der = der - 1;
-			}
-			if (pos != der) {
-				aux = listaQ.obtenerNodo(pos);
-				listaQ.asignar(listaQ.obtenerNodo(der), pos);
-				listaQ.asignar(aux, der);
-				String tmpEstilo = seriesQ.getData().get(der).getNode().getStyle();
-				String tmpEstiloDos = seriesQ.getData().get(pos).getNode().getStyle();
-				seriesQ.getData().get(pos).getNode().setStyle(tmpEstilo);
-				seriesQ.getData().get(der).getNode().setStyle(tmpEstiloDos);
-				seriesQ.getData().get(pos).setYValue(listaQ.obtenerNodo(pos));
-				seriesQ.getData().get(der).setYValue(listaQ.obtenerNodo(der));
-				pos = der;
-				while (listaQ.obtenerNodo(pos) >= listaQ.obtenerNodo(izq) && pos != izq) {
-					izq = izq + 1;
-				}
-				if (pos != izq) {
-					band = true;
-					aux = listaQ.obtenerNodo(pos);
-					listaQ.asignar(listaQ.obtenerNodo(izq), pos);
-					listaQ.asignar(aux, izq);
-					tmpEstilo = seriesQ.getData().get(izq).getNode().getStyle();
-					tmpEstiloDos = seriesQ.getData().get(pos).getNode().getStyle();
-					seriesQ.getData().get(pos).getNode().setStyle(tmpEstilo);
-					seriesQ.getData().get(izq).getNode().setStyle(tmpEstiloDos);
-					seriesQ.getData().get(pos).setYValue(listaQ.obtenerNodo(pos));
-					seriesQ.getData().get(izq).setYValue(listaQ.obtenerNodo(izq));
-					pos = izq;
-				}
-				break;
-			}
-		}
-		if (pos - 1 > ini) {
-			reduceRecursivo(ini, pos - 1);
-		}
-		if (fin > (pos + 1)) {
-			reduceRecursivo(pos + 1, fin);
-		}
-	}
-	public int reduceItertivo(int ini, int fin, int pos) throws IndiceFueraDeRango {
-		int izq = 0;
-		int der = 0;
-		int aux = 0;
-		boolean band;
-		izq = ini;
-		der = fin;
-		pos = ini;
-		band = true;
-		while (band == true) {
-			while (listaQI.obtenerNodo(pos) <= listaQI.obtenerNodo(der) && pos != der) {
-				der--;
-			}
-			if (pos == der) {
-				band = false;
-			} else {
-				aux = listaQI.obtenerNodo(pos);
-				listaQI.asignar(listaQI.obtenerNodo(der), pos);
-				listaQI.asignar(aux, der);
-				String tmpEstilo = seriesQI.getData().get(der).getNode().getStyle();
-				String tmpEstiloDos = seriesQI.getData().get(pos).getNode().getStyle();
-				seriesQI.getData().get(pos).getNode().setStyle(tmpEstilo);
-				seriesQI.getData().get(der).getNode().setStyle(tmpEstiloDos);
-				seriesQI.getData().get(pos).setYValue(listaQI.obtenerNodo(pos));
-				seriesQI.getData().get(der).setYValue(listaQI.obtenerNodo(der));
-				pos = der;
-				while (listaQI.obtenerNodo(pos) >= listaQI.obtenerNodo(izq) && pos != izq) {
-					izq++;
-				}
-				if (pos == izq) {
-					band = false;
-				} else {
-					aux = listaQI.obtenerNodo(pos);
-					listaQI.asignar(listaQI.obtenerNodo(izq), pos);
-					listaQI.asignar(aux, izq);
-					tmpEstilo = seriesQI.getData().get(izq).getNode().getStyle();
-					tmpEstiloDos = seriesQI.getData().get(pos).getNode().getStyle();
-					seriesQI.getData().get(pos).getNode().setStyle(tmpEstilo);
-					seriesQI.getData().get(izq).getNode().setStyle(tmpEstiloDos);
-					seriesQI.getData().get(pos).setYValue(listaQI.obtenerNodo(pos));
-					seriesQI.getData().get(izq).setYValue(listaQI.obtenerNodo(izq));
-					pos = izq;
-				}
-			}
-			break;
-		}
-		return pos;
 	}
 }
